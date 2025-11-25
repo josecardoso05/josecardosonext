6f8c4c6653faa80276e83c1f94952e472d24879d
josecardoso <josebritocardoso05@gmail.com>
2025-11-25 16:24:41 +0000

    botao na pagina tecnologia

--- app/tecnologia/page.tsx
+++ app/tecnologia/page.tsx
@@ -1,11 +1,12 @@
 import tecnologias from "@/app/data/tecnologias.json"
 import TecnologiaDetailsCard from "@/components/TecnologiaDetailsCard/TecnologiaDetailsCard";
+import Link from "next/link";
 
 interface TecnologiaProps {
     indice: number;
 }
 
-export default function TecnologiaPage({indice = 1}: TecnologiaProps) {
+export default function TecnologiaPage({ indice = 1 }: TecnologiaProps) {
     const tecnologia = tecnologias[indice]
 
     return (
@@ -15,6 +16,11 @@ export default function TecnologiaPage({indice = 1}: TecnologiaProps) {
                     description={tecnologia.description}
                 />
             </p>
+            <Link href="/tecnologias">
+                <button style={{ padding: "10px 20px", background: "blue", color: "white", borderRadius: "8px" }}>
+                    Voltar
+                </button>
+            </Link>
         </div>
     )
 }
