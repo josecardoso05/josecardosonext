8ec3a87c286192eddbd49875d9459f462362c5d8
josecardoso <josebritocardoso05@gmail.com>
2025-11-27 15:06:07 +0000

    lab 10, contador feito

---
+++ app/contador/page.tsx
@@ -0,0 +1,10 @@
+import Contador from "@/components/Contador/Contador"
+
+export default function ContadorPage() {
+    return (
+        <div>
+            <Contador/>
+        </div>
+
+    )
+}
---
+++ components/Contador/Contador.tsx
@@ -0,0 +1,60 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+export default function Contador() {
+    const [value, setValue] = useState(() => {
+        const valueStored = localStorage.getItem('value')
+        return valueStored ? parseInt(valueStored) : 0
+    })
+
+    const [history, setHistory] = useState<number[]>([])
+
+    useEffect(() => {
+        localStorage.setItem('value', `${value}`);
+    }, [value]);
+
+    function mudaCor() {
+        if (value >= 0 && value <= 3) {
+            return "text-red-500"
+        }
+        if (value >= 4 && value <= 7) {
+            return "text-yellow-500"
+        }
+        if (value >= 8 && value <= 10) {
+            return "text-green-500"
+        }
+    }
+
+    return (
+        <div>
+            <p className={`m-2 ${mudaCor()}`}>{value}</p>
+            <button className="bg-blue-500 rounded-lg mx-2"
+                onClick={() => {
+                    value < 10 ? setValue(value + 1) : value
+                    setHistory([...history, value])
+
+                }}
+            >
+                Aumenta
+            </button>
+            <button className="bg-blue-500 rounded-lg mx-2"
+                onClick={() => {
+                    value > 0 ? setValue(value - 1) : value
+                    setHistory([...history, value])
+                }}
+                
+            >
+                Diminui</button>
+            <button className="bg-blue-500 rounded-lg"
+                onClick={() => setValue(0)}
+            >
+                Reset</button>
+
+            <ul>
+                Histórico:
+                {history.map((c, i) => (<li key={i}>{c}</li>))}
+            </ul>
+        </div>
+
+    )
+}
